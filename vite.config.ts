ceb54a2de9d45060b73941555d6fea8478fb5ae0
sanae <sanae.k1986@gmail.com>
2026-04-16 06:25:30 +0100

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { reactRouter } from '@react-router/dev/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
